c69da7a8798a870d7de47c880fb17e22c43a30f5
commit-ana <ana.lima25@iesb.edu.br>
2026-06-19 16:28:17 -0300

    configurando app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+//Importando as rotas
+const emprestimoRoutes = require('./routes/emprestimos');
+const equipamentoRoutes = require('./routes/equipamentos');
+const usuarioRoutes = require('./routes/usuarios');
+
 const sequelize = require('./database');
 
 require('./models');
@@ -10,6 +15,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//Registrando as rotas
+app.use('/usuarios', usuarioRoutes);
+app.use('/equipamentos', equipamentoRoutes);
+app.use('/emprestimos', emprestimoRoutes);
 
 
 app.get('/', (req,res)=>{
@@ -27,8 +37,15 @@ sequelize.sync()
     console.log("Erro banco:", erro);
 });
 
+//Iniciando o servidor
+if(process.env.NODE_ENV !== "test"){
 
-app.listen(3000, ()=>{
-    console.log("Servidor rodando na porta 3000");
-});
+    app.listen(3000, ()=>{
+        console.log("Servidor rodando na porta 3000");
+    });
+
+}
+
+//Exportando o app para ser usado nos testes
+module.exports = app;
 
